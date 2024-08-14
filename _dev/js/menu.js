81ad7e63705a8dcfcae73fd7ab0d174dfc4f6734
Pablo Noel <pablo@next-solutions.ca>
2024-08-14 16:13:27 -0400

    Menu styles + interaction

--- _dev/js/menu.js
+++ _dev/js/menu.js
@@ -2,14 +2,43 @@
     document.addEventListener('DOMContentLoaded', () => {
         const menu_toggler = document.getElementById('menuToggler');
         const menu_content = document.getElementById('menuContent');
+        const body = document.getElementById('body');
+        const header = document.getElementById('header');
+        const scroll = document.getElementById('scroll');
         
-        function toggleMenuClass() {
-            menu_content.classList.toggle('open');
-            console.log('toggled')
+        function openMenu(){
+
+            setTimeout(function(){
+                menu_content.classList.add('open');
+            }, 300);
+
+            body.classList.add('locked');
+            header.classList.add('open');
+            scroll.classList.add('open');
+          }
+
+        function closeMenu(){
+
+            setTimeout(function(){
+                body.classList.remove('locked');
+                header.classList.remove('open');
+            }, 300)
+
+            setTimeout(function(){
+                scroll.classList.remove('open');
+            }, 200)
+
+            menu_content.classList.remove('open');
         }
         
-        menu_toggler.addEventListener('click', toggleMenuClass);
-        
-        console.log('loaded')
+        menu_toggler.addEventListener('click', function(){
+            var item = body.classList.contains('locked')
+            if ( item ){
+            closeMenu()
+            } else {
+            openMenu()
+            }
+        });
+
     });
 })();
