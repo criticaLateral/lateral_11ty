bc505802840304fdeabf17000334014887dd2be7
Pablo Noel <pablo@next-solutions.ca>
2024-09-26 11:06:59 -0300

    update Menu

--- build/assets/js/transitions.js
+++ build/assets/js/transitions.js
@@ -1,15 +1,64 @@
 const container = document.getElementById('body');
 
+const menu_toggler = document.getElementById('menuToggler');
+const menu_content = document.getElementById('menuContent');
+const body = document.getElementById('body');
+const header = document.getElementById('header');
+const scroll = document.getElementById('scroll');
+
+function openMenu(){
+
+  setTimeout(function(){
+      menu_content.classList.add('open');
+  }, 300);
+
+  body.classList.add('locked');
+  header.classList.add('open');
+  scroll.classList.add('open');
+}
+
+function closeMenu(){
+
+  setTimeout(function(){
+      body.classList.remove('locked');
+      header.classList.remove('open');
+  }, 300)
+
+  setTimeout(function(){
+      scroll.classList.remove('open');
+  }, 200)
+
+  menu_content.classList.remove('open');
+}
+
+menu_toggler.addEventListener('click', function(){
+  var item = body.classList.contains('locked')
+  console.log('clicked');
+  if ( item ){
+      closeMenu()
+  } else {
+      openMenu()
+  }
+});
+
 barba.init({
   transitions: [{
     name: 'opacity-transition',
-    enter() {
-      container.classList.remove('locked');
+    leave() {
+      closeMenu(),
       anime({
         targets: body,
         opacity: [0,1],
         easing: 'easeInOutQuad',
-        duration: 600
+        duration: 600,
+      });
+    },
+    enter() {
+      anime({
+        targets: body,
+        opacity: [0,1],
+        easing: 'easeInOutQuad',
+        duration: 600,
       });
     }
   }]
